4b057df0f5dd1422096ea5fc7325d1b8aa772a36
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2026-02-21 04:25:03 +0530

    added

---
+++ foreach.java
@@ -0,0 +1,10 @@
+public class foreach {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie", "Diana"};
+
+        // Using a for-each loop to iterate through the array
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
