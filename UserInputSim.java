31dd55247f5b2848ea63218c4a3c721df156c683
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2026-02-21 03:22:05 +0530

    Modiefied

---
+++ UserInputSim.java
@@ -0,0 +1,17 @@
+public class UserInputSim {
+
+  public static void main(String[] args) {
+
+    int attempts = 0;
+    boolean isLoggedIn = false;
+    
+    while (!isLoggedIn && attempts < 3) {
+      
+      ++attempts;
+
+      }
+
+      System.out.println("Login attempts:" + attempts);
+
+  }
+}
