2fbdcff3d2160c93e1df982853beb0f82ef97118
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2026-02-21 04:29:20 +0530

    added

---
+++ SumFirstN.java
@@ -0,0 +1,11 @@
+public class SumFirstN {
+   
+  public static void main(String[] args) {
+
+    int n = 5 , sum = 0;
+    for (int i = 1; i <= n; i++) {
+      sum += i;
+    }
+    System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+  }
+}
