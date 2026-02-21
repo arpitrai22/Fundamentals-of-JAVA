f60068c624b3f36caf1de0735405b485831288fd
arpitrai22 <90815334+arpitrai22@users.noreply.github.com>
2026-02-22 04:00:49 +0530

    added

---
+++ ArrayDeclare.java
@@ -0,0 +1,19 @@
+public class ArrayDeclare {
+  
+  public static void main(String[] args) {
+
+    int[] arr = new int[5];
+
+    int i;
+
+    for(i = 0; i<=4; i++) {
+
+      arr[i] = i;
+      System.out.println(arr[i]);
+
+    }
+
+
+  }
+
+}
